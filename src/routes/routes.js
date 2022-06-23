414a4b3923e148e251b358a264f9cd4436fbd185
Brijeshkumar Yadav <by9714322072@gmail.com>
2022-06-23 18:23:10 +0530

    [OPTIMISE] the code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+
 import { Route, Routes } from 'react-router-dom';
 
-import Layout from '../Views/Layout/Layout';
+// If we use lazy with suspense then only particular file data will load.
 
-import Category from '../Views/Category/Category';
-import Login from '../Views/LogIn/LogIn';
-import SignUp from '../Views/SignUp/SignUp';
-import Product from '../Views/Product/Product';
-import CheckOut from '../Views/CheckOut/CheckOut';
-import SingleProduct from '../Views/Product/SingleProduct';
+const Layout = lazy(() => import('../Views/Layout/Layout'));
+
+const Category = lazy(() => import('../Views/Category/Category'));
+const Login = lazy(() => import('../Views/LogIn/LogIn'));
+const SignUp = lazy(() => import('../Views/SignUp/SignUp'));
+const Product = lazy(() => import('../Views/Product/Product'));
+const CheckOut = lazy(() => import('../Views/CheckOut/CheckOut'));
+const SingleProduct = lazy(() => import('../Views/Product/SingleProduct'));
 
 const routes = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Category />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/signup' element={<SignUp />}></Route>
-          <Route path='/product' element={<Product />}></Route>
-          <Route
-            path='/product/single/:name'
-            element={<SingleProduct />}
-          ></Route>
-          <Route path='/checkout' element={<CheckOut />}></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Category />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/signup' element={<SignUp />}></Route>
+            <Route path='/product' element={<Product />}></Route>
+            <Route
+              path='/product/single/:name'
+              element={<SingleProduct />}
+            ></Route>
+            <Route path='/checkout' element={<CheckOut />}></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
